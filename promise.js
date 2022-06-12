c2df56b21109b2447dba199cb6ce2d2f0f285015
Manarul22 <manarul0022@gmail.com>
2022-06-12 19:29:16 +0700

    push challenge-07

--- promise.js
+++ promise.js
@@ -1,7 +1,16 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (emosi) => {
+  let t1 = await promiseTheaterIXX();
+  let t2 = await promiseTheaterVGC();
+  let combined = t1.concat(t2);
+  let result = 0;
+  combined.map((t1) => {
+    t1.hasil === emosi && (result += 1);
+    return result;
+  })
+};
 
 module.exports = {
   promiseOutput,
